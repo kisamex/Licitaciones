927e7e67fec17f2d4991e2b453d9815e8f5982ff
kisamex <kalisto_34_@hotmail.com>
2018-08-31 22:40:27 -0300

    Arreglo de pep e501

--- polls/views.py
+++ polls/views.py
@@ -11,37 +11,39 @@ def index(request):
 
 def add_proveedores(request):
     if request.method == 'POST':
-    	form = add_proveedoresform(request.POST)
-    	If form.is_valid():
-			form.save()
-			messages.success(request, 'Se creo el Proveedor!')
-			return redirect('index')
-		else:
-			messages.warning(request, 'Completa bien los datos GATO')
-			return render(request, 'proveedores/add_proveedores.html', {'form':form}) 
-	else:
-		form = add_proveedoresform()
-	return render(request, 'proveedores/add_proveedores.html', {'form':form})
+        form = add_proveedoresform(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Se creo el Proveedor!')
+            return redirect('index')
+        else:
+            messages.warning(request, 'Completa bien los datos GATO')
+        return render(request, 'proveedores/add_proveedores.html', {'form': form})
+    else:
+        form = add_proveedoresform()
+    return render(request, 'proveedores/add_proveedores.html', {'form': form})
+
 
 def add_licitaciones(request):
-	if request.method == 'POST':
-		form = add_licitacionesform(request.POST)
-		if form.is_valid():
-			form.save()
-			messages.success(request, 'Se creo la licitacion!')
-			return redirect('index')
-		else:
-			messages.warning(request, 'Completa bien los datos GATO')
-			return render(request, 'licitaciones/add_licitaciones.html', {'form':form}) 
-	else:
-		form = add_licitacionesform()
-	return render(request, 'licitaciones/add_licitaciones.html', {'form':form})
+    if request.method == 'POST':
+        form = add_licitacionesform(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Se creo la licitacion!')
+            return redirect('index')
+        else:
+            messages.warning(request, 'Completa bien los datos GATO')
+        return render(request, 'licitaciones/add_licitaciones.html', {'form': form})
+    else:
+        form = add_licitacionesform()
+    return render(request, 'licitaciones/add_licitaciones.html', {'form': form})
+
 
 def list_proveedores(request):
-	list_proveedores = proveedor.objects.all()
-	return render(request, 'proveedores/list_proveedores.html', {'list_proveedores':list_proveedores})
+    list_proveedores = proveedor.objects.all()
+    return render(request, 'proveedores/list_proveedores.html', {'list_proveedores': list_proveedores})
 
 
 def list_licitaciones(request):
-	list_licitaciones = licitaciones.objects.all()
-	return render(request, 'licitaciones/list_licitaciones.html', {'list_licitaciones':list_licitaciones})
+    list_licitaciones = licitaciones.objects.all()
+    return render(request, 'licitaciones/list_licitaciones.html', {'list_licitaciones': list_licitaciones})
